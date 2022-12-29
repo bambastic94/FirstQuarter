5bd74f280a548fce1bee63e53625cbc4a29f2f72
Dmitriy <Wonderful_days@mail.ru>
2022-12-29 19:38:48 +0300

    Add ArrayLength, CreateArray and ThreeSymbols methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] words = { "Friday", "Hello", "Two", "Hi!", "Greetings", "Yo!", "=P", "Lesson", "Test", "Job" };
+
+int ArrayLength(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] CreateArray(int count)
+{
+    string[] array = new string[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+    return array;
+}
+
+string[] ThreeSymbols(string[] array)
+{
+    string[] line = new string[ArrayLength(array)];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            line[j] = array[i];
+            j++;
+        }
+    }
+    return line;
+}
